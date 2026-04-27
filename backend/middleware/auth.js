82c2bbe5e7953ece9ea2b14ad82df8f969acb247
unknown <ishimwejennymiriotta@gmail.com>
2026-04-27 17:01:44 +0200

    Finalizing the auth routes in backend

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,17 @@
 const jwt = require("jsonwebtoken")
-const token = req.headers.authorization?.split(' ')[1]
-
-if(!token){
-    return res.status(401).send
+const auth = (req, res,next) => {
+    const token = req.headers.authorization?.split(' ')[1]  
+    if(!token){
+        return res.status(401).send("No token, acess denied")
+    }
+    try{
+        const decoded =  jwt.verify(token,process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    }
+    catch{
+        return res.status(401).send("Invalid token")
+    }  
 }
+
+module.exports = auth
---
+++ backend/routes/auth.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+
+router.post('/register',async(req,res)=>{
+    const {name, email,password,confirmPassword} = req.body
+    if(password != confirmPassword){
+        return res.status(400).send("Passwords do not match")
+    }
+    if(await User.findOne({email})){
+        return res.status(400).send("User already exists!")
+    }
+    const hashedPassword = await bcrypt.hash(password,10)
+    const newUser = new User({name,email,password:hashedPassword})
+    await newUser.save()
+    res.status(201).send('User resistered successfully')
+})
+
+router.post('/login',async(req,res)=>{
+    const {email,password,rememberMe}= req.body
+    const isUser = await User.findOne({email})
+    if(!isUser){
+        return res.status(400).send("User not found")
+    }
+    const isMatch = await bcrypt.compare(password,isUser.password)
+    if(!isMatch){
+        return res.status(400).send("Incorrect password")
+    }
+    const token = jwt.sign(
+        {id:isUser._id},
+        process.env.JWT_SECRET,
+        {expiresIn:rememberMe ? '30d' : '1d'}
+    )
+    res.json({token})
+})
+
+module.exports = router
