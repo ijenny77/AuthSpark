83d1adba7579c1f43f366d1c48e2160c2559240a
unknown <ishimwejennymiriotta@gmail.com>
2026-04-27 22:57:27 +0200

    Intergration of backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
 const port = process.env.PORT || 3000
-
+app.use(cors())
 app.use(express.json())
 const authRoutes = require("./routes/auth")
 app.get("/",(req,res)=>{
